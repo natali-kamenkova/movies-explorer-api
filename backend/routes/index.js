29911417e8d084091d25dec004663be679db9e0b
Natali Kamenkova <natalikamenkova@gmail.con>
2023-04-20 21:51:41 +0300

    NotFound error

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const NotFound = require('../errors/NotFound');
 const { auth } = require('../middlewares/auth');
 
 router.use('/movies', auth, movieRouter);
 router.use('/users', auth, userRouter);
 
-router.use('/*', auth, (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+router.use('/*', auth, (req, res, next) => {
+  next(new NotFound('Страница не найдена'));
 });
 
 module.exports = router;
